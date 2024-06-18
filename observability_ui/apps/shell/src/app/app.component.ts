e3ce99ac204dca125b2b68a78b2b59f37e318287
Luis Trinidad <ltrinidad@datakitchen.io>
2024-06-18 17:54:29 -0400

    feat(ui): add support for replacing the header component
    Makes it possible to render a component injected through Module Federation
    to replace the default header component.

--- observability_ui/apps/shell/src/app/app.component.ts
+++ observability_ui/apps/shell/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional } from '@angular/core';
 import { AppVersionService } from './app-version/app-version.service';
 import { SessionService, EntitiesResolver, ProjectStore } from '@observability-ui/core';
 import { filter, tap } from 'rxjs';
+import { ComponentType } from '@angular/cdk/portal';
+import { REPLACEMENT_TOKENS } from '../app/config';
 
 @Component({
   selector: 'shell-root',
@@ -12,12 +14,12 @@ export class AppComponent {
 
   isAuth$ = this.sessionService.isLoggedIn$;
   currentProject$ = this.projectStore.current$;
-
   constructor(
     private sessionService: SessionService,
     private appVersion: AppVersionService,
     private entities: EntitiesResolver,
     private projectStore: ProjectStore,
+    @Optional() @Inject(REPLACEMENT_TOKENS.HeaderComponent) public headerComponent?: ComponentType<any>,
   ) {
     this.appVersion.currentVersion$.subscribe((version) => {
       console.log({version});
--- observability_ui/apps/shell/src/app/config.ts
+++ observability_ui/apps/shell/src/app/config.ts
@@ -1,10 +1,24 @@
 import { Manifest as BaseManifest, RemoteConfig as BaseRemoteConfig } from '@angular-architects/module-federation';
+import { ComponentType } from '@angular/cdk/portal';
+import { InjectionToken } from '@angular/core';
+
+export type ComponentReplacement = {
+  token: string;
+  exposedComponent: string;
+  exposedComponentName: string;
+};
 
 export type RemoteConfig = BaseRemoteConfig & {
   routePath: string;
   remoteName: string;
   exposedModule: string;
   exposedModuleName: string;
+  replacements: ComponentReplacement[];
 };
 
 export type Manifest = BaseManifest<RemoteConfig>;
+
+export const REPLACEMENT_TOKENS = {
+  ProjectsMenu: new InjectionToken<any>('ProjectsMenu'),
+  HeaderComponent: new InjectionToken<ComponentType<any> | undefined>('HeaderComponent'),
+};
