84681f47e50a548701a0ab981eb2b3eb2c3eb0f5
Luis Trinidad <ltrinidad@datakitchen.io>
2024-08-19 16:41:16 -0400

    refactor(be): filter and paginate entities list in a subquery
    Joins and selecting multiple fields for both counting and paginating
    caused MySQL to count rows one by one or ignore indexes. This commit
    changes the paginated query to filter, order and paginate in a subquery
    that selects only the entity ID; and joins the subquery with the
    original query.

--- common/tests/unit/entity_services/helpers/test_list_rules.py
+++ common/tests/unit/entity_services/helpers/test_list_rules.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock
+from unittest.mock import ANY, Mock, MagicMock
 
 import pytest
 from werkzeug.datastructures import MultiDict
@@ -20,21 +20,29 @@ def mocked_entity_class():
 
 @pytest.fixture()
 def mocked_query():
-    # .paginate() (and other query methods in general) doesn't return a list until iterated on, but this is good enough for the test
-    paginate = Mock(return_value=list())
     query = Mock()
-    query.paginate = paginate
-    query.order_by = Mock(return_value=query)
+    subquery = Mock()
+    results_query = MagicMock()
+
+    query.model.select.return_value = subquery
+    query.clone = Mock(return_value=results_query)
+
+    subquery.where.return_value = subquery
+    subquery.order_by.return_value = subquery
+    subquery.paginate.return_value = subquery
+    subquery.alias.return_value = subquery
+
+    results_query.join.return_value = results_query
+    results_query.order_by.return_value = results_query
+
     yield query
 
 
 @pytest.fixture()
-def mocked_query_with_data():
-    # .paginate() (and other query methods in general) doesn't return a list until iterated on, but this is good enough for the test
-    paginate = Mock(return_value=[Mock(), Mock()])
-    query = Mock()
-    query.paginate = paginate
-    query.order_by = Mock(return_value=query)
+def mocked_query_with_data(mocked_query):
+    query = mocked_query
+    results_query = query.clone.return_value
+    results_query.__iter__.return_value = [Mock(), Mock()]
     yield query
 
 
@@ -80,11 +88,24 @@ def test_from_params_extras():
 
 @pytest.mark.unit
 def test_page_get_paginated_results(mocked_field, mocked_entity_class, mocked_query):
+    mocked_subquery = mocked_query.model.select.return_value
+    mocked_results_query = mocked_query.clone.return_value
     rules = ListRules.from_params(MultiDict())
+
     result = Page[mocked_entity_class].get_paginated_results(mocked_query, mocked_field, rules)
+
     mocked_field.asc.assert_called_once()
-    mocked_query.paginate.assert_called_once()
-    mocked_query.count.assert_called_once()
+
+    mocked_subquery.where.assert_called_once_with(mocked_query._where)
+    mocked_subquery.order_by.assert_called_once_with(rules.order_by_field(mocked_field))
+    mocked_subquery.paginate.assert_called_once_with(rules.page, rules.count)
+    mocked_subquery.alias.assert_called_once()
+    mocked_subquery.count.assert_called_once_with(clear_limit=True)
+
+    assert mocked_results_query._where is None
+    mocked_results_query.join.assert_called_once_with(mocked_subquery, join_type=ANY, on=ANY)
+    mocked_results_query.order_by.assert_called_once_with(rules.order_by_field(mocked_field))
+
     assert type(result) == Page
 
 
@@ -102,6 +123,6 @@ def test_page_len(mocked_field, mocked_entity_class, mocked_query_with_data):
 def test_page_iter(mocked_field, mocked_entity_class, mocked_query_with_data):
     rules = ListRules.from_params(MultiDict())
     result = Page[mocked_entity_class].get_paginated_results(mocked_query_with_data, mocked_field, rules)
-    count = len(mocked_query_with_data.paginate())
+    count = len(list(mocked_query_with_data.clone.return_value))
     r = [r for r in result]
     assert len(r) == count
