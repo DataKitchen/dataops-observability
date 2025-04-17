5d081b432dc2c8349448fc3a552696858b8ed766
Aarthy Adityan <aarthy@datakitchen.io>
2025-04-17 17:49:10 -0400

    fix(security): make cors headers restrictive

--- common/tests/unit/flask_ext/test_cors.py
+++ common/tests/unit/flask_ext/test_cors.py
@@ -1,3 +1,4 @@
+import flask
 import pytest
 from flask import Response
 
@@ -7,8 +8,30 @@ CORS_HEADER_TUPLE: tuple[str, ...] = tuple(CORS_HEADERS.keys())
 
 
 @pytest.mark.unit
-def test_headers_added():
-    response = Response()
-    CORS.set_cors_headers(response)
-    for header_name in CORS_HEADER_TUPLE:
-        assert header_name in response.headers
+def test_headers_added_matching_domain():
+    test_app = flask.Flask("test_flask_app")
+    cors = CORS()
+
+    matching_domains = ["https://xyz.com", "https://test.mydomain.com", "http://localhost:8080"]
+    for domain in matching_domains:
+        with test_app.test_request_context(headers={"Origin": domain}):
+            response = Response()
+            cors.set_cors_headers(response)
+            assert response.headers["Access-Control-Allow-Origin"] is domain
+            for header_name in CORS_HEADER_TUPLE:
+                assert header_name in response.headers
+
+
+@pytest.mark.unit
+def test_no_headers_other_domain():
+    test_app = flask.Flask("test_flask_app")
+    cors = CORS()
+
+    matching_domains = ["https://abc.com", "https://test.mydomain.com.io", "http://localhost"]
+    for domain in matching_domains:
+        with test_app.test_request_context(headers={"Origin": domain}):
+            response = Response()
+            cors.set_cors_headers(response)
+            assert "Access-Control-Allow-Origin" not in response.headers
+            for header_name in CORS_HEADER_TUPLE:
+                assert header_name not in response.headers
